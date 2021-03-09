a4345d42bce33974eae32373d5e5ceb4a886fd4c
trombonist852 <trombonist852@gmail.com>
2021-03-09 21:35:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,33 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Modify Router Name
+sed -i 's/OpenWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
+
+#Modify Default Network Interface
+sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3 eth4 eth5/g' package/base-files/files/etc/board.d/99-default_network
+sed -i '/ucidef_set_interface_wan/s/eth1/eth0/g' package/base-files/files/etc/board.d/99-default_network
+
+#Modify Default Password
+sed -i 's#root::0:0:99999:7:::#root:$1$fe9OTETj$lEJwiQW4hDxi/GNj4JUlC1:18679:0:99999:7:::#g' package/base-files/files/etc/shadow
+
+#Modify Boot Disk(for X86)
+#sed -i 's/hd0/hd3/g' target/linux/x86/image/grub-efi.cfg
+
+#Del package
+rm -rf package/lean/qBittorrent
+rm -rf package/lean/qt5
+rm -rf package/diy/OpenAppFilter
+rm -rf openwrt/package/lean/luci-app-filetransfer
+
+# Install to emmc
+git clone https://github.com/tuanqing/install-program package/install-program
+
+# Theme
+svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-19.07
+
+#Add Package
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/wltc2005/openwrt-redsocks2.git package/redsocks2
