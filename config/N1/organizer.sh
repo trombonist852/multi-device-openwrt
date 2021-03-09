f7986c5b171263cc5329583af077afceaaaa6f90
trombonist852 <trombonist852@gmail.com>
2021-03-09 14:34:46 +0800

    Create organizer.sh

---
+++ config/N1/organizer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Build Firmware"
+
+git clone https://github.com/ylqjgm/mknop.git mknop
+mkdir -p mknop/openwrt
+cp -r -f dist/openwrt-armvirt-64-default-rootfs.tar.gz mknop/openwrt/
+cd mknop
+sudo ./gen_openwrt -d -k 5.4.98 -m phicomm-n1
+mv out/phicomm-n1/*.img ../dist/
