f8a5727450bdd4031d27c67d21a6dab9481a5fd6
trombonist852 <trombonist852@gmail.com>
2021-03-11 00:55:03 +0800

    Update organizer.sh

--- config/N1/organizer.sh
+++ config/N1/organizer.sh
@@ -7,4 +7,5 @@ mkdir -p mknop/openwrt
 cp -r -f dist/openwrt-armvirt-64-default-rootfs.tar.gz mknop/openwrt/
 cd mknop
 sudo ./gen_openwrt -s 1536 -d -k 5.4.98 -m phicomm-n1
-mv out/phicomm-n1/*.img ../dist/
+gzip out/phicomm-n1/*.img
+mv out/phicomm-n1/*.img.gz ../dist/
