df14a47de7f29104ca36850a219835cc90cf9a05
trombonist852 <trombonist852@gmail.com>
2021-03-13 17:49:36 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -20,8 +20,8 @@
 sed -i 's/OpenWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
 
 #Modify Default Network Interface
-sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3 eth4 eth5/g' package/base-files/files/etc/board.d/99-default_network
-sed -i '/ucidef_set_interface_wan/s/eth1/eth0/g' package/base-files/files/etc/board.d/99-default_network
+#sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3 eth4 eth5/g' package/base-files/files/etc/board.d/99-default_network
+#sed -i '/ucidef_set_interface_wan/s/eth1/eth0/g' package/base-files/files/etc/board.d/99-default_network
 
 #Modify Default Password
 sed -i 's#root::0:0:99999:7:::#root:$1$fe9OTETj$lEJwiQW4hDxi/GNj4JUlC1:18679:0:99999:7:::#g' package/base-files/files/etc/shadow
