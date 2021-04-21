fbdda7fc96e04018865756b83bfe6486a2744648
jiawm <42288069+jiawm@users.noreply.github.com>
2021-04-22 06:58:32 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -17,14 +17,14 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #Modify Router Name
-sed -i 's/OpenWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/Trb.Corp/g' package/base-files/files/bin/config_generate
 
 #Modify Default Network Interface
 #sed -i '/ucidef_set_interface_lan/s/eth0/eth1 eth2 eth3 eth4 eth5/g' package/base-files/files/etc/board.d/99-default_network
 #sed -i '/ucidef_set_interface_wan/s/eth1/eth0/g' package/base-files/files/etc/board.d/99-default_network
 
 #Modify Default Password
-sed -i 's#root::0:0:99999:7:::#root:$1$fe9OTETj$lEJwiQW4hDxi/GNj4JUlC1:18679:0:99999:7:::#g' package/base-files/files/etc/shadow
+#sed -i 's#root::0:0:99999:7:::#root:$1$fe9OTETj$lEJwiQW4hDxi/GNj4JUlC1:18679:0:99999:7:::#g' package/base-files/files/etc/shadow
 
 #Modify Boot Disk(for X86)
 #sed -i 's/hd0/hd3/g' target/linux/x86/image/grub-efi.cfg
