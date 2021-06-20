657e75a31de59b4dadc7b17908e0fc389d4ea245
trombonist852 <trombonist852@gmail.com>
2021-06-20 15:57:58 +0800

    Update diy-r4s-1.sh

--- Scripts/diy-r4s-1.sh
+++ Scripts/diy-r4s-1.sh
@@ -44,15 +44,17 @@ rm -rf package/diy/luci-app-dockerman
 #git clone https://github.com/tuanqing/install-program package/install-program
 
 # Theme
-#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/custom/luci-theme-infinityfreedom
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon-19.07
+rm -rf package/custom/luci-theme-argon-19.07/luasrc/view/themes/argon/footer.htm
+rm -rf package/custom/luci-theme-argon-19.07/luasrc/view/themes/argon/header.htm
+svn export https://github.com/trombonist852/custom/trunk/footer.htm package/custom/luci-theme-argon-19.07/luasrc/view/themes/argon/
+svn export https://github.com/trombonist852/custom/trunk/header.htm package/custom/luci-theme-argon-19.07/luasrc/view/themes/argon/
 
 #Add Package
 git clone https://github.com/tty228/luci-app-serverchan.git package/custom/serverchan
 git clone https://github.com/wltc2005/openwrt-redsocks2.git package/custom/redsocks2
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/custom/argon-config
 git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/custom/autotimeset
-git clone https://github.com/trombonist852/custom.git package/custom/filetransfer
 git clone https://github.com/lwz322/k3screenctrl.git package/custom/k3screenctrl
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/custom/luci-app-k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl_build.git package/custom/k3screenctrl_build
@@ -62,3 +64,4 @@ git clone https://github.com/godros/luci-app-godproxy.git package/custom/luci-ap
 #mkdir package/base-files/files/etc/modules.d
 #svn export https://github.com/maxlicheng/phicomm-n1-wireless/trunk/wireless_enable package/base-files/files/etc/modules.d/
 svn checkout https://github.com/openwrt/luci/trunk/applications/luci-app-dockerman package/diy/luci-app-dockerman
+svn checkout https://github.com/trombonist852/custom/trunk/luci-app-filetransfer package/custom/luci-app-filetransfer
